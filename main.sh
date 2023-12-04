e65b3cc8e1d4ba0cdb4b90340b56357d999da706
Ari Herman <ari.j.herman@gmail.com>
2023-12-04 14:55:18 -0700

    wip

--- main.sh
+++ main.sh
@@ -3,5 +3,5 @@
 # FILEPATH: /home/ari/learning/main.sh
 
 # Allocate 4 nodes using srun and run main.py
-srun -N 4 python main.py --version original >> log_original.out &&
-srun -N 4 python main.py --version alternate >> log_alternate.out &&
+srun -N 1 -c 10 python main.py --version original >> log_original.out &&
+srun -N 1 -c 10 python main.py --version alternate >> log_alternate.out &&
