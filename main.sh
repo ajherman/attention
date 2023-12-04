0e825c51caa9df133fac7697ac644c6e2b372a96
Ari Herman <ari.j.herman@gmail.com>
2023-12-04 13:54:28 -0800

    Loads miniconda and activates env in main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # FILEPATH: /home/ari/learning/main.sh
-
+module purge
+module load miniconda3
+source activate /vast/home/ajherman/miniconda3/envs/pytorch
 # Allocate 4 nodes using srun and run main.py
 srun -N 4 python main.py --version original >> log_original.out &&
 srun -N 4 python main.py --version alternate >> log_alternate.out &&
