6ff85bc83e0cf6b6f2fb13ebee7a2695cbf219fc
Ari Herman <ari.j.herman@gmail.com>
2023-12-04 23:00:13 -0800

    Adds original version for comparison

--- plot.py
+++ plot.py
@@ -1,1 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the CSV files
+df1 = np.genfromtxt('original.csv', delimiter=',')
+# df2 = np.genfromtxt('alternate.csv', delimiter=',')
+
+# # Plot the data
+plt.plot(df1)
+# plt.plot(df2)
+print(df1)
+# Label the axes
+plt.xlabel('time')
+plt.ylabel('error')
+
+# Add a legend
+plt.legend()
+
+# Save the figure
+plt.savefig('/home/ari/learning/comparison.png')
+
+# Show the plot
+plt.show()
+
 
