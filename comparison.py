0a45373da5ea8702d01a5c2b27c9c92468f0d738
Ari Herman <ari.j.herman@gmail.com>
2023-12-06 13:31:40 -0800

    wip

--- comparison.py
+++ comparison.py
@@ -32,15 +32,6 @@ if not os.path.exists(file_path):
 with open('shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-# # here are all the unique characters that occur in this text
-# chars = sorted(list(set(text)))
-# vocab_size = len(chars)
-# # create a mapping from characters to integers
-# stoi = { ch:i for i,ch in enumerate(chars) }
-# itos = { i:ch for i,ch in enumerate(chars) }
-# encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-# decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
 # Get char list
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
