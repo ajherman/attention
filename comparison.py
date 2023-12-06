4501f9cf5ee58dc6425222f8c678dfb80413bd6f
Ari Herman <ari.j.herman@gmail.com>
2023-12-06 09:55:06 -0800

    test

--- comparison.py
+++ comparison.py
@@ -117,69 +117,70 @@ class Transformer(nn.Module):
             idx=torch.cat((idx,idx_next),dim=1)
         return idx
 
-# class GPTLanguageModel(nn.Module):
+class Transformer2(nn.Module):
 
-#     def __init__(self):
-#         super().__init__()
-#         # each token directly reads off the logits for the next token from a lookup table
-#         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-#         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-#         self.blocks = nn.Sequential(*[Block(n_embd,n_head) for _ in range(n_layer)])
-#         self.ln_f = nn.LayerNorm(n_embd) # final layer norm
-#         self.lm_head = nn.Linear(n_embd, vocab_size)
+    def __init__(self):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd,n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
-#         # better init, not covered in the original GPT video, but important, will cover in followup video
-#         self.apply(self._init_weights)
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
 
-#     def _init_weights(self, module):
-#         if isinstance(module, nn.Linear):
-#             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-#             if module.bias is not None:
-#                 torch.nn.init.zeros_(module.bias)
-#         elif isinstance(module, nn.Embedding):
-#             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-#     def forward(self, idx, targets=None):
-#         B, T = idx.shape
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
 
-#         # idx and targets are both (B,T) tensor of integers
-#         tok_emb = self.token_embedding_table(idx) # (B,T,C)
-#         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
 
-#         x = tok_emb + pos_emb # (B,T,C)
-#         x = self.blocks(x) # (B,T,C)
-#         x = self.ln_f(x) # (B,T,C)
-#         logits = self.lm_head(x) # (B,T,vocab_size)
+        x = tok_emb + pos_emb # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
 
-#         if targets is None:
-#             loss = None
-#         else:
-#             B, T, C = logits.shape
-#             logits = logits.view(B*T, C)
-#             targets = targets.view(B*T)
-#             loss = F.cross_entropy(logits, targets)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
 
-#         return logits, loss
+        return logits, loss
 
-#     def generate(self, idx, max_new_tokens):
-#         # idx is (B, T) array of indices in the current context
-#         for _ in range(max_new_tokens):
-#             # crop idx to the last block_size tokens
-#             idx_cond = idx[:, -block_size:]
-#             # get the predictions
-#             logits, loss = self(idx_cond)
-#             # focus only on the last time step
-#             logits = logits[:, -1, :] # becomes (B, C)
-#             # apply softmax to get probabilities
-#             probs = F.softmax(logits, dim=-1) # (B, C)
-#             # sample from the distribution
-#             idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-#             # append sampled index to the running sequence
-#             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
-#         return idx
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
 
-model = Transformer(n_embd,vocab_size)
-# model = GPTLanguageModel()
+# model = Transformer(n_embd,vocab_size)
+model = Transformer2()
+
 m = model.to(device)
 # print the number of parameters in the model
 print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
