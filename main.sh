f73e14e0a249ddfd22c01c165ecc1c1aac54de86
Ari Herman <ari.j.herman@gmail.com>
2023-12-06 17:21:14 -0800

    creates wrapper for main?

---
+++ main.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash -l
+
+#SBATCH --job-name=main
+#SBATCH --time 10:00:00
+#SBATCH -N 5
+#SBATCH -p shared-gpu
+#module load miniconda3
+#source activate /vast/home/ajherman/miniconda3/envs/pytorch
+
+srun -N 1 -n 1 -c 6 -o log_original.out --open-mode=append ./main_wrapper.sh --version original --filepath original.csv
+srun -N 1 -n 1 -c 6 -o log_alternate.out --open-mode=append ./main_wrapper.sh --version alternate --filepath alternate.csv
+
+
