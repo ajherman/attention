7eed9acf38d9ebdd4b554605bc383fc2601c44d4
Ari Herman <ari.j.herman@gmail.com>
2023-12-15 16:38:49 -0800

    add bug reproduce file

---
+++ bug.py
@@ -0,0 +1,8 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import transformers
+from datasets import load_dataset
+
+data_cache_dir = "datasets" #"/home/ari/Desktop"
+dataset = load_dataset("nRuaif/tinystories-gpt4",cache_dir=data_cache_dir,split='train')
+dataloader = DataLoader(dataset, batch_size=64)
