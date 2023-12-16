d679ec1bdceda7029353d50143d09d282949b65f
Ari Herman <ari.j.herman@gmail.com>
2023-12-15 18:51:50 -0700

    wip

--- bug.py
+++ bug.py
@@ -3,6 +3,6 @@ from torch.utils.data import Dataset, DataLoader
 import transformers
 from datasets import load_dataset
 
-data_cache_dir = "datasets" #"/home/ari/Desktop"
+data_cache_dir = "/ram/tmp" #"/home/ari/Desktop"
 dataset = load_dataset("nRuaif/tinystories-gpt4",cache_dir=data_cache_dir,split='train')
 dataloader = DataLoader(dataset, batch_size=64)
