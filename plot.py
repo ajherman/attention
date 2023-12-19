a6b28c335471a6de5fd76b2f13255bf8322cf957
Ari Herman <ari.j.herman@gmail.com>
2023-12-19 09:14:38 -0800

    Makes general plotting file that take cla.

--- plot.py
+++ plot.py
@@ -1,43 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import os
 
-# Read the CSV files
-original = np.genfromtxt('original.csv', delimiter=',')
-rms = np.genfromtxt('rms.csv', delimiter=',')
-mix = np.genfromtxt('mix.csv', delimiter=',')
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir',type=str,default='.', help='Directory of CSV files')
+parser.add_argument('--files', nargs='+', help='List of CSV files')
+parser.add_argument('--save-name',type=str, help='Where to save the plots')
+# parser.add_argument('--save-name',type=str, help='Name of the plot')
+colors=['blue','red','green','orange','purple','brown','pink','gray','olive','cyan']
+args = parser.parse_args()
 
-# # Plot the data
-plt.plot(original[:,0], label='Original train',color='blue')
-plt.plot(original[:,1], linestyle='--', label='Original test',color='blue')
-plt.plot(rms[:,0], label='RMS train',color='red')
-plt.plot(rms[:,1], linestyle='--', label='RMS test',color='red')
-plt.plot(mix[:,0], label='Mix train',color='green')
-plt.plot(mix[:,1], linestyle='--', label='Mix test',color='green')
+for idx,f_name in enumerate(args.files):
+    if f_name.endswith('.csv'):
+        f_name = f_name[:-4]
+    file_path = args.dir+'/'+f_name+'.csv'
+    if not os.path.exists(file_path):
+        print(f"File '{file_path}' does not exist. Skipping...")
+    else:
+        data = np.genfromtxt(file_path, delimiter=',')
+        plt.plot(data[:,0], label=f_name+' train',linestyle='--',color=colors[idx])
+        plt.plot(data[:,1], label=f_name+' test',color=colors[idx])
+
+
+# # Read the CSV files
+# original = np.genfromtxt('original.csv', delimiter=',')
+# rms = np.genfromtxt('rms.csv', delimiter=',')
+# mix = np.genfromtxt('mix.csv', delimiter=',')
+
+# # # Plot the data
+# plt.plot(original[:,0], label='Original train',color='blue')
+# plt.plot(original[:,1], linestyle='--', label='Original test',color='blue')
+# plt.plot(rms[:,0], label='RMS train',color='red')
+# plt.plot(rms[:,1], linestyle='--', label='RMS test',color='red')
+# plt.plot(mix[:,0], label='Mix train',color='green')
+# plt.plot(mix[:,1], linestyle='--', label='Mix test',color='green')
 
 # Label the axes
 plt.xlabel('time')
 plt.ylabel('error')
+plt.title('Error vs Time')
 
 # Add a legend
 plt.legend()
 
 # Save the figure
-plt.savefig('comparison.png')
+plt.savefig(args.dir+'/'+args.save_name+'.png')
 
 # Show the plot
 plt.show()
 
-# Label the axes
-plt.xlabel('time')
-plt.ylabel('error')
-
-# Add a legend
-plt.legend()
-
-# Save the figure
-plt.savefig('comparison.png')
-
-# Show the plot
-plt.show()
 
 
