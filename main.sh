fe135ac1940721853dca49b5f60eb33863288c7e
Ari Herman <ari.j.herman@gmail.com>
2023-12-20 16:44:05 -0800

    wip

--- main.sh
+++ main.sh
@@ -11,6 +11,8 @@ cores=20
 
 #srun -N 1 -n 1 -c $cores -o regular.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 3 --filepath regular.csv &
 #srun -N 1 -n 1 -c $cores -o rectified.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 5 --filepath rectified.csv &
-srun -N 1 -n 1 -c $cores -o log.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 6 --filepath log.csv &
+# srun -N 1 -n 1 -c $cores -o log.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 6 --filepath log.csv &
+srun -N 1 -n 1 -c $cores -o mine.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 7 --filepath mine.csv &
+
 
 #srun -N 1 -n 1 -c $cores -o base.out --open-mode=append ./main_wrapper.sh --block-type 0 --filepath base.csv &
