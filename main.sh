578dc45ecda5563f6a01db4846406c9fb869dcc1
Ari Herman <ari.j.herman@gmail.com>
2023-12-29 14:03:55 -0700

    wip

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@
 
 #SBATCH --job-name=main
 #SBATCH --time 10:00:00
-#SBATCH -N 2
+#SBATCH -N 5
 #SBATCH -p shared-gpu
 #module load miniconda3
 #source activate /vast/home/ajherman/miniconda3/envs/pytorch
-cores=40
+cores=20
 
 #srun -N 1 -n 1 -c $cores -o regular.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 3 --filepath regular.csv &
 #srun -N 1 -n 1 -c $cores -o rectified.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 5 --filepath rectified.csv &
