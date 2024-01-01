5c5fcaf3ade25fae1cce94ba92d615806670b0c0
Ari Herman <ari.j.herman@gmail.com>
2024-01-01 15:02:57 -0800

    wip

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import transformers
 from datasets import load_dataset
 #from torch.utils.tensorboard import SummaryWriter
 from tokenizers import Tokenizer
-from transformers import GPT2Tokenizer, GPT2Model
+from transformers import GPT2Tokenizer, GPT2Model, AutoTokenizer
 
 data_cache_dir = "/ram/tmp"
 dataset = 'stories' # 'shakespeare'
@@ -73,13 +73,16 @@ if dataset == 'shakespeare':
         return out
     
 elif dataset == 'stories':
-    vocab_size=50258
+    # vocab_size=50258
     train_set = load_dataset("nRuaif/tinystories-gpt4",cache_dir=data_cache_dir,split='train')
     train_loader = DataLoader(train_set, batch_size=64)
     test_set = load_dataset("nRuaif/tinystories-gpt4",cache_dir=data_cache_dir,split='test')
     test_loader = DataLoader(test_set, batch_size=64)
     # tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
     tokenizer = AutoTokenizer.from_pretrained("georgeyw/TinyStories-tokenizer-10k")
+    vocab_size = tokenizer.vocab_size
+    print(vocab_size)
+    assert(0)
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     decode = tokenizer.decode
     # def get_batch(split,block_size):
