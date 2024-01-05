612433fb41d7f63a5601655612e8a6215a16b551
Ari Herman <ari.j.herman@gmail.com>
2024-01-04 16:57:08 -0800

    wip

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default=device, help='Specify the device')
     parser.add_argument('--n-itrs', type=int, default=20001, help='Specify the number of iterations')
     parser.add_argument('--dropout', type=float, default=0.2, help='Specify the dropout')
-    # parser.add_argument('--vocab-size', type=int, default=vocab_size, help='Specify the vocab size')
+    parser.add_argument('--vocab-size', type=int, default=vocab_size, help='Specify the vocab size')
     parser.add_argument('--block-type', type=int, default=3, help='Specify the version')
     parser.add_argument('--filepath', type=str,default='original.csv', help='Specify the file path')
     parser.add_argument('--dataset', type=str,default='shakespeare', help='Specify the dataset')
