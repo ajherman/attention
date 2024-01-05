3612e6780f8adcb1055af64691bb28fd57d8c1b0
Ari Herman <ari.j.herman@gmail.com>
2024-01-04 20:00:27 -0700

    wip

--- main.sh
+++ main.sh
@@ -16,7 +16,7 @@ cores=20
 #srun -N 1 -n 1 -c $cores -o base.out --open-mode=append ./main_wrapper.sh --batch-size 32 --block-type 0 --filepath base.csv &
 
 # For TinyStories version
-srun -N 1 -n 1 -c $cores -o base.out --open-mode=append ./main_wrapper.sh --block-type 0 --block-size 100 --batch-size 8 --dm 384 --h 6 --N 8 --dataset stories --filepath base.csv & #--vocab-size 50258 &
+srun -N 1 -n 1 -c $cores -o base.out --open-mode=append ./main_wrapper.sh --block-type 0 --block-size 40 --batch-size 2 --dm 384 --h 6 --N 8 --dataset stories --filepath base.csv & #--vocab-size 50258 &
 
 #srun -N 1 -n 1 -c $cores -o base.out --open-mode=append ./main_wrapper.sh --block-type 0 --block-size 100 --dm 384 --h 8 --lr 5e-4 --filepath base.csv #--vocab-size 50258 &
 
