acea8945f1f73e9f0fbf01b0607ed2a80877af78
Ari Herman <ari.j.herman@gmail.com>
2024-01-29 19:50:25 -0800

    wip

--- main.sh
+++ main.sh
@@ -32,9 +32,9 @@ version=0
 
 for norm_type in {layer,rms}
 do
-for rectify in {False,}
+for rectify in {0,1}
 do
-for post_norm in {False,}
+for post_norm in {0,1}
 do
 
 name="norm_${norm_type}_rectify_${rectify}_post_norm_${post_norm}"
