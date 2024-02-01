b9e80cb7d5fbdf38893f65dc04910f82462c56d0
Ari Herman <ari.j.herman@gmail.com>
2024-01-31 22:15:16 -0800

    wip

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ if __name__ == '__main__':
             # cls_token_id = tokenizer.cls_token_id
             prompt = "The meaning of life is"
             prompt = encode(prompt, return_tensors="pt").to(device)
-            print("\nSample: \n", decode(prompt), '\n\n')
+            print("\nSample: \n", decode(prompt[0]), '\n\n')
             assert(0)
             n = len(prompt[0])
             idx = torch.zeros((1, args.block_size), device=device, dtype=torch.long)
